2b035a5c731326521f184aaeb85e1d4d5fef3d38
Helvin Rymer <55415802+Helvin159@users.noreply.github.com>
2025-03-11 19:57:03 -0400

    trying

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
   }
 };
