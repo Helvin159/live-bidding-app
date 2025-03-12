91e5267341eb39331dcacf41521c6f942924a18a
Helvin Rymer <55415802+Helvin159@users.noreply.github.com>
2025-03-11 20:04:39 -0400

    trying

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from 'next';
-import path from 'path';
 
-const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;
-  }
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
